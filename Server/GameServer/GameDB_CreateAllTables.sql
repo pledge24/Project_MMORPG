91f6660f07d81fb3487d9947fc5e6312462eea1f
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-08-09 04:07:00 +0900

    🩹 fix: 테이블 관계 설정

--- Server/GameServer/GameDB_CreateAllTables.sql
+++ Server/GameServer/GameDB_CreateAllTables.sql
@@ -1,6 +1,7 @@
 -- GameDB에서 사용하는 모든 테이블
 
 -- 1. Characters: 캐릭터 기본 정보
+DROP TABLE IF EXISTS Characters;
 CREATE TABLE Characters(
     character_id        BIGINT IDENTITY(1, 1) PRIMARY KEY,
     user_id             BIGINT NOT NULL,
@@ -14,7 +15,8 @@ GO
 
 
 -- 2. CharacterState: 캐릭터의 마지막 상태
-CREATE TABLE CharacterState(
+DROP TABLE IF EXISTS CharacterLastState;
+CREATE TABLE CharacterLastState(
     character_id        BIGINT PRIMARY KEY,
     exp                 BIGINT NOT NULL DEFAULT 0,
     cur_hp              INT NOT NULL DEFAULT 0,
@@ -26,25 +28,40 @@ CREATE TABLE CharacterState(
     pos_y               FLOAT NOT NULL DEFAULT 0.0,
     pos_z               FLOAT NOT NULL DEFAULT 0.0,
     rot_yaw             FLOAT NOT NULL DEFAULT 0.0,
-    gold                BIGINT NOT NULL DEFAULT 1000
+    gold                BIGINT NOT NULL DEFAULT 1000,
+
+    FOREIGN KEY (character_id) REFERENCES Characters(character_id)
+    ON DELETE CASCADE
 );
 GO
 
 
 -- 3. CharacterInventory: 캐릭터 인벤토리 정보
+DROP TABLE IF EXISTS CharacterInventory;
 CREATE TABLE CharacterInventory(
     character_id        BIGINT NOT NULL,
     item_id             INT NOT NULL,
     slot_id             INT NOT NULL,
-    quantity            INT NOT NULL DEFAULT 1
+    quantity            INT NOT NULL DEFAULT 1,
+
+    FOREIGN KEY (character_id) REFERENCES Characters(character_id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (character_id, slot_id)
 );
 GO
 
 
 -- 4. CharacterEquipment: 캐릭터 장착 정보
+DROP TABLE IF EXISTS CharacterEquipment;
 CREATE TABLE CharacterEquipment(
     character_id        BIGINT NOT NULL,
     item_id             INT NOT NULL,
     slot_id             INT NOT NULL, -- 장착 부위 ID(ex. 0=투구, 1=상의, 2=하의, 3=무기)
+
+    FOREIGN KEY (character_id) REFERENCES Characters(character_id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (character_id, slot_id)
 );
 GO
