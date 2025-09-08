9136aa8d3549971b4b595a5a45172b21e77f31e5
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-09-08 23:18:59 +0900

    🩹 fix: 저장 안해서 푸시 안 된 부분 추가 커밋

--- Server/GameServer/Inventory.h
+++ Server/GameServer/Inventory.h
@@ -18,7 +18,7 @@ public:
     bool addItem(OUT Protocol::Slot* updatedSlot, Protocol::Item& itemInstance, int32 count = 1);
     bool addItem(OUT Protocol::Slot* updatedSlot, int32 templateId, int32 count = 1);
 
-    bool removeItem(OUT Protocol::Slot* updatedSlot, Protocol::Slot* targetSlot, int32 count = 1);
+    bool removeItem(OUT Protocol::Slot* updatedSlot, Protocol::Slot* slot, int32 count = 1);
 
     int32 findFirstAvailableSlotId(Protocol::ItemType type, int32 templateId);
 
@@ -27,5 +27,6 @@ public:
 private:
     unordered_map<Protocol::ItemType, vector<Protocol::Slot*>> lookupTableMappings;
     unordered_map<Protocol::ItemType, vector<bool>> dirtyFlagsMappings;
+    unordered_map<Protocol::SlotType, Protocol::ItemType> slotTypeToItemTypeMappings;
 };
 
